bd3396ed778c221af692f2b08556b6de6dbc73e5
Jaydeep Sambangi <jcsambangi@gmail.com>
2018-12-14 05:04:14 -0500

    Travis ask, I do

--- testing/travis.sql
+++ testing/travis.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `serial_pin` (
 ALTER TABLE `dashr`
   ADD PRIMARY KEY (`pin`);
 ALTER TABLE `dashr`
-  ADD PRIMARY KEY (`dashr_create_time`)
+  ADD PRIMARY KEY (`dashr_create_time`);
 ALTER TABLE `serial_pin`
   ADD PRIMARY KEY (`serial`);
-INSERT INTO TABLE `serial_pin` VALUES (261122326, 435), (261791206, 9307)
+INSERT INTO TABLE `serial_pin` VALUES (261122326, 435), (261791206, 9307);
