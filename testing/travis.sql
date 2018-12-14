9e4bbe128e7dfdd64327eb8390a326c582358cf1
Jaydeep Sambangi <jcsambangi@gmail.com>
2018-12-14 05:00:02 -0500

    More sql changes

--- testing/travis.sql
+++ testing/travis.sql
@@ -6,14 +6,14 @@ CREATE TABLE IF NOT EXISTS `dashr` (
   `dashr_create_time` datetime NOT NULL,
   `session_time` datetime
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-ALTER TABLE `dashr`
-  ADD PRIMARY KEY (`pin`);
-ALTER TABLE `dashr`
-  ADD PRIMARY KEY (`dashr_create_time`)
 CREATE TABLE IF NOT EXISTS `serial_pin` (
   `serial` int(11) NOT NULL,
   `pin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE `dashr`
+  ADD PRIMARY KEY (`pin`);
+ALTER TABLE `dashr`
+  ADD PRIMARY KEY (`dashr_create_time`)
 ALTER TABLE `serial_pin`
   ADD PRIMARY KEY (`serial`);
 INSERT INTO TABLE `serial_pin` VALUES (261122326, 435), (261791206, 9307)
