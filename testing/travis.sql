24182b715988067f5607ea34b6cd203671b94bfb
Jaydeep Sambangi <jcsambangi@gmail.com>
2018-12-14 05:07:14 -0500

    Travis refuses multiple primary keys

--- testing/travis.sql
+++ testing/travis.sql
@@ -12,8 +12,6 @@ CREATE TABLE IF NOT EXISTS `serial_pin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `dashr`
   ADD PRIMARY KEY (`pin`);
-ALTER TABLE `dashr`
-  ADD PRIMARY KEY (`dashr_create_time`);
 ALTER TABLE `serial_pin`
   ADD PRIMARY KEY (`serial`);
 INSERT INTO TABLE `serial_pin` VALUES (261122326, 435), (261791206, 9307);
